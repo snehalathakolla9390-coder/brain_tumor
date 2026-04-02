b5e442caed329a827fdeaa22990ca962d177d0e4
Sneha Kolla <your-email@gmail.com>
2026-04-02 11:53:45 +0530

    Support Environment Variables in settings.py for safe and flexible deployment

--- Detecting_and_Diagnosing_Brain_Tumor_MRI_Image/settings.py
+++ Detecting_and_Diagnosing_Brain_Tumor_MRI_Image/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wa7ghuntm30^@4-0imff)&#+@n*%!_i29_r!m6+azr7!%w8$st'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-wa7ghuntm30^@4-0imff)&#+@n*%!_i29_r!m6+azr7!%w8$st')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
